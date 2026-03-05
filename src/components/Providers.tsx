ebdfb354108e2b57a87bb4cb8ab86e80f41eea64
serag52 <seragibrahim52@gmail.com>
2026-03-05 20:33:31 +0200

    Update

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,12 +4,6 @@ import { SessionProvider } from 'next-auth/react';
 import { ToastProvider } from './Toast';
 import ServiceWorkerDevCleanup from '@/components/ServiceWorkerDevCleanup';
 import ServiceWorkerRecovery from '@/components/ServiceWorkerRecovery';
-import { usePWAAuth } from '@/hooks/usePWAAuth';
-
-function PWASessionProvider({ children }: { children: React.ReactNode }) {
-  usePWAAuth();
-  return <>{children}</>;
-}
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -17,7 +11,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
       <ToastProvider>
         <ServiceWorkerDevCleanup />
         <ServiceWorkerRecovery />
-        <PWASessionProvider>{children}</PWASessionProvider>
+        {children}
       </ToastProvider>
     </SessionProvider>
   );
